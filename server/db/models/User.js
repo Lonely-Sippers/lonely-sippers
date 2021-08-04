794a3f2d8e10d073fb884e6537157cd8d9853092
Ashley Buono <81730498+ashleygbuono@users.noreply.github.com>
2021-08-04 09:45:34 -0400

    user model added

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,74 +1,49 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const axios = require('axios');
+const Sequelize = require('sequelize');
+const { STRING, INTEGER, UUIDV4, DATE } = Sequelize;
+const db = require('../db');
 
-const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
+  id: {
+      type: INTEGER,
+      allowNull: false
+  },
+  cartId: {
+      type: UUIDV4
+  },
   username: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false
+      type: STRING,
+      unique: true
   },
   password: {
-    type: Sequelize.STRING,
+      type: STRING
+  },
+  email: {
+      type: STRING,
+      unique: true,
+      validate: {
+          isEmail: true
+      }
+  },
+  firstName: {
+      type: STRING
+  },
+  lastName: {
+      type: STRING
+  },
+  createdAt: {
+      type: DATE,
+      validate: {
+          isDate: true
+      }
+  }, 
+  orderId: {
+    //needs Order model
+    type: UUIDV4
   }
-})
 
-module.exports = User
+});
 
-/**
- * instanceMethods
- */
-User.prototype.correctPassword = function(candidatePwd) {
-  //we need to compare the plain version to an encrypted version of the password
-  return bcrypt.compare(candidatePwd, this.password);
-}
-
-User.prototype.generateToken = function() {
-  return jwt.sign({id: this.id}, process.env.JWT)
-}
-
-/**
- * classMethods
- */
-User.authenticate = async function({ username, password }){
-    const user = await this.findOne({where: { username }})
-    if (!user || !(await user.correctPassword(password))) {
-      const error = Error('Incorrect username/password');
-      error.status = 401;
-      throw error;
-    }
-    return user.generateToken();
-};
-
-User.findByToken = async function(token) {
-  try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = User.findByPk(id)
-    if (!user) {
-      throw 'nooo'
-    }
-    return user
-  } catch (ex) {
-    const error = Error('bad token')
-    error.status = 401
-    throw error
-  }
-}
-
-/**
- * hooks
- */
-const hashPassword = async(user) => {
-  //in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
-    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-  }
-}
-
-User.beforeCreate(hashPassword)
-User.beforeUpdate(hashPassword)
-User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+module.exports = User;
+//allowNull on just id vs other attributes too, another way to determine users with account vs without?
+//Role or Access model -- diff. between user vs admin
