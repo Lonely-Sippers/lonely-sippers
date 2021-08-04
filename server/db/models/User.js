b6bb5758841769caa6a293b6d4a3bb93197eeced
Brian Smolin <briansmolin@yahoo.com>
2021-08-04 13:15:45 -0400

    commit being weird

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,49 +1,102 @@
 const Sequelize = require('sequelize');
-const { STRING, INTEGER, UUIDV4, DATE } = Sequelize;
 const db = require('../db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+const { STRING, INTEGER, DATE } = Sequelize; //double check UUID formatting. wasn't working and was throwing errors
 
+const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
-  id: {
-      type: INTEGER,
-      allowNull: false
-  },
-  cartId: {
-      type: UUIDV4
-  },
+  //ID
   username: {
-      type: STRING,
-      unique: true
+    type: STRING,
+    unique: true,
+    allowNull: false,
   },
   password: {
-      type: STRING
+    type: STRING,
   },
+  cartId: {
+    type: INTEGER,
+  },
+  //some of the fields below probably should have an allowNull:false
+  //   but I'm leaving it off for the time being so everything builds without modifying the seed file
   email: {
-      type: STRING,
-      unique: true,
-      validate: {
-          isEmail: true
-      }
+    type: STRING,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   firstName: {
-      type: STRING
+    type: STRING,
   },
   lastName: {
-      type: STRING
+    type: STRING,
   },
-  createdAt: {
-      type: DATE,
-      validate: {
-          isDate: true
-      }
-  }, 
   orderId: {
     //needs Order model
-    type: UUIDV4
-  }
-
+    type: INTEGER,
+  },
+  //I don't think we need a createdAt field. Sequelize does that for us automatically
 });
 
 module.exports = User;
-//allowNull on just id vs other attributes too, another way to determine users with account vs without?
-//Role or Access model -- diff. between user vs admin
+
+// //allowNull on just id vs other attributes too, another way to determine users with account vs without?
+// //Role or Access model -- diff. between user vs admin
+
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function (candidatePwd) {
+  //we need to compare the plain version to an encrypted version of the password
+  return bcrypt.compare(candidatePwd, this.password);
+};
+
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
+
+/**
+ * classMethods
+ */
+User.authenticate = async function ({ username, password }) {
+  const user = await this.findOne({ where: { username } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password');
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
+};
+
+User.findByToken = async function (token) {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
+    if (!user) {
+      throw 'nooo';
+    }
+    return user;
+  } catch (ex) {
+    const error = Error('bad token');
+    error.status = 401;
+    throw error;
+  }
+};
+
+/**
+ * hooks
+ */
+const hashPassword = async (user) => {
+  //in case the password has been changed, we want to encrypt it with bcrypt
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+  }
+};
+
+User.beforeCreate(hashPassword);
+User.beforeUpdate(hashPassword);
+User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
