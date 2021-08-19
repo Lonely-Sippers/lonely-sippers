7d3f1f06cd5690400ea4d8b866d5d0cf4919ca9e
Brian Smolin <briansmolin@yahoo.com>
2021-08-18 20:03:53 -0400

    grinding

--- client/components/home/Admin_SingleProduct.js
+++ client/components/home/Admin_SingleProduct.js
@@ -1,70 +1,67 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { getSingleProduct } from "../../store/products";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getSingleProduct } from '../../store/products';
 
-class _AdminSingleProduct extends React.Component {
-    constructor(props) {
-      super(props);
+const _AdminSingleProduct = (props) => {
+  console.log('PROPS', props);
+  const id = props.history.match.params.id * 1;
+  console.log('ID!!', id);
+  const product =
+    props.products.find((prod) => {
+      // console.log(prod);
+      return prod.id === id;
+    }) || {};
 
-      this.state = {
-          product: {}
-      };
-    }
-  async componentDidMount() {
-    const { id } = this.props.match.params;
-    await this.props.getSingleProduct(id);
-  }
+  console.log('singleProduct', product);
 
-  render() {
-    const product = this.props.product.products || {};
-    console.log("singleProduct", product);
+  return (
+    <div>
+      <div className="pt-20">
+        <img
+          src={product.image_URL}
+          alt=""
+          className="mx-auto rounded-lg"
+          width="10%"
+        />
 
-    return (
-      <div>
-        <div className="pt-20">
-          <img
-            src={product.image_URL}
-            alt=""
-            className="mx-auto rounded-lg"
-            width="10%"
-          />
+        <div className="px-8">
+          <div className="md:flex space-x-7 py-8">
+            <h3 className="font-semibold">{product.category}</h3>
+            <h4>{product.alcohol_type}</h4>
+          </div>
 
-          <div className="px-8">
-            <div className="md:flex space-x-7 py-8">
-              <h3 className="font-semibold">{product.category}</h3>
-              <h4>{product.alcohol_type}</h4>
-            </div>
+          {/* <p>{product.description}</p> */}
 
-            {/* <p>{product.description}</p> */}
+          <h4 className="">Country of Origin: {product.region}</h4>
 
-            <h4 className="">Country of Origin: {product.region}</h4>
-
-            <div>
-              <h4>Alcohol Percentage: {product.alcohol_percentage}</h4>
-            </div>
-            <h4 className="mb-16">Price: ${product.price}</h4>
-            <div className="md:flex md:justify-between py-8 relative bottom-0 wider">
-              <button className="btn transition-colors duration-300  mt-4 lg:mt-0  rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
-                Edit This Product
-              </button>
-              <button
-                className="btn transition-colors duration-300  mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
-                onClick={() => {}}
-              >
-                Delete
-              </button>
-            </div>
+          <div>
+            <h4>Alcohol Percentage: {product.alcohol_percentage}</h4>
+          </div>
+          <h4 className="mb-16">Price: ${product.price}</h4>
+          <div className="md:flex md:justify-between py-8 relative bottom-0 wider">
+            <button className="btn transition-colors duration-300  mt-4 lg:mt-0  rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
+              Edit This Product
+            </button>
+            <button
+              className="btn transition-colors duration-300  mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+              onClick={() => {}}
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-const mapStateToProps = (product = {}) => ({
-  product,
-});
+const mapStateToProps = ({ products }, history) => {
+  return {
+    products,
+    history,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
